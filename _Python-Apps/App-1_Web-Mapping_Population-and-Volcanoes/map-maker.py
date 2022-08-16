1604964773a1fe7255a5de332d6a54e92b7804d8
xroadtraveler <xroadtraveler@gmail.com>
2022-08-16 16:38:45 -0700

    replace default map layer, add marker point, add feature group

--- _Python-Apps/App-1_Web-Mapping_Population-and-Volcanoes/map-maker.py
+++ _Python-Apps/App-1_Web-Mapping_Population-and-Volcanoes/map-maker.py
@@ -1,3 +1,9 @@
 import folium
 map = folium.Map(location=[47.60, -122.33], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[47.9, -122.7], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+
 map.save("Map1.html")
