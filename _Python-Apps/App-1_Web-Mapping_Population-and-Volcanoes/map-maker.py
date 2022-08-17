948b6908d12e3dc3b684af3e32beb300bfeb0e57
xroadtraveler <xroadtraveler@gmail.com>
2022-08-16 17:24:20 -0700

    extract .csv data, add lat./lon. map markers for 62 volcanoes

--- _Python-Apps/App-1_Web-Mapping_Population-and-Volcanoes/map-maker.py
+++ _Python-Apps/App-1_Web-Mapping_Population-and-Volcanoes/map-maker.py
@@ -1,11 +1,27 @@
+import pandas
 import folium
+
+
+# This section extracts latitude and longitude from 'Volcanoes.csv'
+data = pandas.read_csv("Volcanoes.csv")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+
+# This section creates our initial map object
 map = folium.Map(location=[47.60, -122.33], zoom_start=6, tiles="Stamen Terrain")
 
+
+# This line creates a single feature group to input all the children into our map
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[48.00, -122.70], [47.00, -121.75]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 
+# This section iterates over coordinates to add Marker Points to map
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+
+# This line adds the whole feature group at once
 map.add_child(fg)
 
 map.save("Map1.html")
