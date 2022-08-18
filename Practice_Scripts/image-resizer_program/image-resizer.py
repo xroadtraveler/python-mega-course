d706840c47b4f5007666dfb27d0448739c8bdead
xroadtraveler <xroadtraveler@gmail.com>
2022-08-17 17:25:31 -0700

    add relative filepath functionality

--- Practice_Scripts/image-resizer_program/image-resizer.py
+++ Practice_Scripts/image-resizer_program/image-resizer.py
@@ -1,6 +1,6 @@
 import cv2
 
-img=cv2.imread("galaxy.jpg", 0)
+img=cv2.imread("./resizer_images/galaxy.jpg", 0)
 
 #print(type(img))
 #print(img)
@@ -9,6 +9,6 @@ img=cv2.imread("galaxy.jpg", 0)
 
 resized_image=cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
 cv2.imshow("Galaxy", resized_image)
-cv2.imwrite("Galaxy_resized.jpg", resized_image)
+cv2.imwrite("./resizer_images/Galaxy_resized.jpg", resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
