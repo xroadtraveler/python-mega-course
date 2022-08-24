101fe1a2860ff6947b96095bd8323400cd074242
xroadtraveler <xroadtraveler@gmail.com>
2022-08-24 05:59:09 -0700

    change 'motion-detector.py' to 'motion_detector.py', keep old ref

---
+++ _Python-Apps/App-2_Webcam-Control-and-Motion-Detect/motion_detector.py
@@ -0,0 +1,78 @@
+import cv2, time, pandas
+from datetime import datetime
+
+# Creates empty variables for later conditional
+first_frame=None
+status_list=[None, None]
+times=[]
+df=pandas.DataFrame(columns=["Start", "End"])
+
+video=cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+    status = 0
+
+    # Creates grayscale version and applies Gaussian blur
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # If first time running, uses first image as the Background Frame
+    if first_frame is None:
+        first_frame=gray
+        continue
+
+    # Creates Delta Frame to calculate differences for motion capture
+    delta_frame=cv2.absdiff(first_frame, gray)
+
+
+    # Creates Threshold Frame to classify differences for motion capture
+    thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+
+
+    # Find threshold contours
+    (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        status = 1
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+    status_list.append(status)
+
+
+    # Records date-times for status changes
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
+
+
+    # Shows all frames
+    cv2.imshow("Capturing", gray)
+    cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
+
+    key=cv2.waitKey(1)
+    #print(gray) # numpy array
+    #print(delta_frame) # numpy array
+
+    if key==ord('q'):
+        if status_list==1:
+            times.append(datetime.now())
+        break
+
+    #print(status) # To check status changes during motion capture
+print(status_list) # To check status change list
+print(times) # To check datetime timestamps
+
+for i in range(0, len(times), 2):
+    df=df.append({"Start":times[i], "End": times[i+1]}, ignore_index=True)
+
+df.to_csv("Times.csv")
+
+video.release()
+cv2.destroyAllWindows
