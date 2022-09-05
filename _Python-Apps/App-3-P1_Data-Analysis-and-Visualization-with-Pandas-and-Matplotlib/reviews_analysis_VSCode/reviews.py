e274f962ce9f3f9d10f0d747dc11b75fd546951f
xroadtraveler <xroadtraveler@gmail.com>
2022-09-04 17:28:48 -0700

    update code to show examples of selecting data

--- _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/reviews.py
+++ _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/reviews.py
@@ -1,3 +1,4 @@
+## Overview of the dataframe
 import pandas
 import matplotlib.pyplot as plt
 
@@ -11,3 +12,29 @@ data.columns
 
 plt.hist(df['Rating'])
 plt.show()
+
+## Selecting data from the dataframe
+### Select a column
+print(data['Rating']) #.mean()
+print()
+
+### Select multiple columns
+print(data[['Course Name', 'Rating']])
+print()
+
+### Selecting a row
+print(data.iloc[3])
+print()
+
+### Selecting multiple rows
+print(data.iloc[1:3])
+print()
+
+### Selecting a section
+print(data[['Course Name', 'Rating']].iloc[1:3])
+print()
+
+### Selecting a cell
+print(data['Timestamp'].iloc[2])
+print(data.at[2, 'Rating'])
+print()
