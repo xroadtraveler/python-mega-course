0d990013298e3113cc6236038a5a521ddd9ecfb3
xroadtraveler <xroadtraveler@gmail.com>
2022-09-05 14:46:07 -0700

    downsample grouped data to show average ratings by month

--- _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/plotting.py
+++ _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/plotting.py
@@ -26,14 +26,34 @@ plt.show()
 
 ### Rating average by week
 # Process data to group by week
+"""
 data['Week'] = data['Timestamp'].dt.strftime('%Y-%U')
 # print(data['Week']) # Check
+"""
 
 # Group by week
+"""
 week_average = data.groupby(['Week']).mean()
 print(week_average)
+"""
 
 # Plot week graph
+"""
 plt.figure(figsize=(25, 3))
 plt.plot(week_average.index, week_average['Rating'])
+plt.show()
+"""
+
+### Rating average by month
+# Process data to group by month
+data['Month'] = data['Timestamp'].dt.strftime('%Y-%m')
+print(data['Month']) # Check
+
+# Group by month
+month_average = data.groupby(['Month']).mean()
+print(month_average)
+
+# Plot month graph
+plt.figure(figsize=(25, 3))
+plt.plot(month_average.index, month_average['Rating'])
 plt.show()
