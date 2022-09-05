b84d1b5726bf4939f242198ab4fef04628ce14e3
xroadtraveler <xroadtraveler@gmail.com>
2022-09-05 10:34:13 -0700

    practice with filtering DataFrames

--- _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/reviews.py
+++ _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/reviews.py
@@ -1,4 +1,4 @@
-## Overview of the dataframe
+## 1. Overview of the dataframe
 import pandas
 import matplotlib.pyplot as plt
 
@@ -6,35 +6,64 @@ data = pandas.read_csv("reviews.csv")
 
 df = pandas.DataFrame(data)
 
+"""
 data.head()
 data.shape
 data.columns
 
 plt.hist(df['Rating'])
 plt.show()
+"""
 
-## Selecting data from the dataframe
+
+## 2. Selecting data from the dataframe
 ### Select a column
+"""
 print(data['Rating']) #.mean()
 print()
+"""
 
 ### Select multiple columns
+"""
 print(data[['Course Name', 'Rating']])
 print()
+"""
 
 ### Selecting a row
+"""
 print(data.iloc[3])
 print()
+"""
 
 ### Selecting multiple rows
+"""
 print(data.iloc[1:3])
 print()
+"""
 
 ### Selecting a section
+"""
 print(data[['Course Name', 'Rating']].iloc[1:3])
 print()
+"""
 
 ### Selecting a cell
+"""
 print(data['Timestamp'].iloc[2])
 print(data.at[2, 'Rating'])
+print()
+"""
+
+## 3. Filtering data based on conditions
+### One condition
+"""
+d2 = data[data['Rating'] > 4]
+print(d2['Rating'])
+print()
+print(d2['Rating'].mean())
+print()
+"""
+
+### Multiple conditions
+print(data[(data['Rating'] > 4) & (data['Course Name'] == 'The Complete Python Course: Build 10 Professional OOP Apps')]['Rating'].mean())
 print()
