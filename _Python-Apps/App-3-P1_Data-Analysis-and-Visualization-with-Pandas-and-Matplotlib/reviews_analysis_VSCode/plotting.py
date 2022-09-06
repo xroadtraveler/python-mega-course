44d6a2ef9ba55393fe5c75fb92d73dc1eeea6556
xroadtraveler <xroadtraveler@gmail.com>
2022-09-05 21:41:22 -0700

    create plot for 'average ratings by course by month'

--- _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/plotting.py
+++ _Python-Apps/App-3-P1_Data-Analysis-and-Visualization-with-Pandas-and-Matplotlib/reviews_analysis_VSCode/plotting.py
@@ -4,6 +4,8 @@ from pytz import utc
 import matplotlib.pyplot as plt
 data = pandas.read_csv("reviews.csv", parse_dates=['Timestamp'])
 
+
+
 ### Rating average/count by day
 # Process data to group by date
 """
@@ -23,6 +25,8 @@ plt.figure(figsize=(25, 3))
 plt.plot(day_average.index, day_average['Rating'])
 plt.show()
 """
+
+
 
 ### Rating average by week
 # Process data to group by week
@@ -44,16 +48,39 @@ plt.plot(week_average.index, week_average['Rating'])
 plt.show()
 """
 
+
+
 ### Rating average by month
 # Process data to group by month
+"""
 data['Month'] = data['Timestamp'].dt.strftime('%Y-%m')
 print(data['Month']) # Check
+"""
 
 # Group by month
+"""
 month_average = data.groupby(['Month']).mean()
 print(month_average)
+"""
 
 # Plot month graph
+"""
 plt.figure(figsize=(25, 3))
 plt.plot(month_average.index, month_average['Rating'])
+plt.show()
+"""
+
+
+
+### Average rating by month by course
+# Process data to group by month AND course name
+data['Month'] = data['Timestamp'].dt.strftime('%Y-%m')
+month_average_crs = data.groupby(['Month', 'Course Name']).mean().unstack() # to graph mean
+# month_average_crs = data.groupby(['Month', 'Course Name'])['Rating'].count().unstack() # to graph count
+# print(month_average_crs[-20:]) # Check
+# print(month_average_crs.index) # Check
+# print(month_average_crs.columns) # Check
+
+# Plot the average per month per course
+month_average_crs.plot(figsize=(25, 8))
 plt.show()
