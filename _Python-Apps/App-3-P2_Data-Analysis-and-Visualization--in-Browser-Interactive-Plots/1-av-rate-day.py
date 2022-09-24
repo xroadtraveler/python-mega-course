d1f9684f93089594fb24aa02128fc11dac7629ac
xroadtraveler <xroadtraveler@gmail.com>
2022-09-24 16:58:14 -0700

    add '1-av-rate-day.py' file with Highcharts functionality

---
+++ _Python-Apps/App-3-P2_Data-Analysis-and-Visualization--in-Browser-Interactive-Plots/1-av-rate-day.py
@@ -0,0 +1,85 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+import matplotlib.pyplot as plt
+
+
+data = pandas.read_csv("reviews.csv", parse_dates=['Timestamp'])
+data['Day'] = data['Timestamp'].dt.date
+day_average = data.groupby(['Day']).mean()
+
+chart_def = """
+{
+    chart: {
+        type: 'spline',
+        inverted: false
+    },
+    title: {
+        text: 'Analysis of Course Reviews'
+    },
+    subtitle: {
+        text: 'Average Rating by Day'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Date'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 2018-01-01 to 2021-03-19.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Average Rating'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0.0 to 5.0.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x}: {point.y}'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Rating',
+        data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+    }]
+}
+"""
+
+def app():
+    wp = jp.QuasarPage() # wp stands for webpage
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+    hc = jp.HighCharts(a=wp, options=chart_def)
+    hc.options.title.text = "Average Rating by Day"
+
+    hc.options.xAxis.categories = list(day_average.index)
+    hc.options.series[0].data = list(day_average['Rating'])
+
+    return wp
+
+jp.justpy(app)
