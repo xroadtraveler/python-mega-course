b82e68ad5fc4137ce8f384a2cbcd581df35eaade
xroadtraveler <xroadtraveler@gmail.com>
2022-10-06 17:24:00 -0700

    add HTML template functionality to web app

--- _Python-Apps/App-4_Web-Development-with-Flask/script1.py
+++ _Python-Apps/App-4_Web-Development-with-Flask/script1.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Homepage goes here!"
+    return render_template("home.html")
 
 @app.route('/about/')
 def about():
-    return "'About' information goes here!"
+    return render_template("about.html") #"'About' information goes here!"
 
 if __name__ == "__main__":
     app.run(debug=True)
