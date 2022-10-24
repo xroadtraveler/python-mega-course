29d3ac4c708399c1478addb3be50994ce7530646
xroadtraveler <xroadtraveler@gmail.com>
2022-10-24 10:48:41 -0700

    update GUI script with labels, entries, listbox, and buttons

--- _Python-Apps/App-5_Book-Inventory-GUI/script1.py
+++ _Python-Apps/App-5_Book-Inventory-GUI/script1.py
@@ -1,14 +1,74 @@
-"""
-A program that stores this book information:
-Title, Author, 
-Year, ISBN
+from tkinter import *
 
-User can:
+# Create window
+window = Tk()
 
-View all records
-Search an entry
-Add entry
-Update entry
-Delete entry
-Close
-"""
+
+# Create labels
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l2 = Label(window, text="Author")
+l2.grid(row=0, column=2)
+
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
+
+
+# Create entries
+title_text = StringVar()
+e1=Entry(window, textvariable=title_text)
+e1.grid(row=0, column=1)
+
+author_text = StringVar()
+e2=Entry(window, textvariable=author_text)
+e2.grid(row=0, column=3)
+
+year_text = StringVar()
+e3=Entry(window, textvariable=year_text)
+e3.grid(row=1, column=1)
+
+isbn_text = StringVar()
+e4=Entry(window, textvariable=isbn_text)
+e4.grid(row=1, column=3)
+
+
+#Create listbox
+list1 = Listbox(window, height=6, width=35)
+list1.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+# Create scrollbar
+sb1 = Scrollbar(window)
+sb1.grid(row=2, column=2, rowspan=6)
+
+# Configure listbox and scrollbar to interact
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+
+# Create buttons
+b1 = Button(window, text="View All", width=12)
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text="Search Entry", width=12)
+b2.grid(row=3, column=3)
+
+b3 = Button(window, text="Add Entry", width=12)
+b3.grid(row=4, column=3)
+
+b4 = Button(window, text="Update Selected", width=12)
+b4.grid(row=5, column=3)
+
+b5 = Button(window, text="Delete Selected", width=12)
+b5.grid(row=6, column=3)
+
+b6 = Button(window, text="Close", width=12)
+b6.grid(row=7, column=3)
+
+
+# Mainloop window
+window.mainloop()
+
