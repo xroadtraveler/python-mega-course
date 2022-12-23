443fd8bc198098ff2a95f7e93462acb5692441a4
xroadtraveler <xroadtraveler@gmail.com>
2022-12-22 17:08:18 -0800

    add SignUpScreen and sign-up button functionality

--- _Python-Apps/App-6_Kivy-Mobile-App/main.py
+++ _Python-Apps/App-6_Kivy-Mobile-App/main.py
@@ -5,9 +5,13 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
 
 class RootWidget(ScreenManager):
+    pass
+
+class SignUpScreen(Screen):
     pass
 
 class MainApp(App):
