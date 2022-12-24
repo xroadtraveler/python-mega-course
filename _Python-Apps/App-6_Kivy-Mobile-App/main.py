e82c4a4954a058432f6de625949c791e32c5cec7
xroadtraveler <xroadtraveler@gmail.com>
2022-12-23 17:53:41 -0800

    add quote output functionality based on user input

--- _Python-Apps/App-6_Kivy-Mobile-App/main.py
+++ _Python-Apps/App-6_Kivy-Mobile-App/main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+import json, glob, random
 from datetime import datetime
+from pathlib import Path
 
 Builder.load_file('design.kv')
 
@@ -41,6 +42,23 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+    
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+
+        available_feelings = [Path(filename).stem for filename in
+                                available_feelings]
+        
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt", encoding='utf-8', errors='ignore') as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+#            print(quotes)
+
+        else:
+            self.ids.quote.text = "Try another feeling."
+#        print(available_feelings)
 
 class MainApp(App):
     def build(self):
