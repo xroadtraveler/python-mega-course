e22ad015fc50f2609cdb90783183670fee3aad41
xroadtraveler <xroadtraveler@gmail.com>
2023-02-10 17:03:41 -0800

    add code to search through properties and extract their price

--- _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
+++ _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
@@ -5,3 +5,7 @@ r=requests.get("https://pythonizing.github.io/data/real-estate/rock-springs-wy/L
 c=r.content
 
 soup=BeautifulSoup(c,"html.parser")
+
+all=soup.find_all("div",{"class":"propertyRow"})
+
+all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
