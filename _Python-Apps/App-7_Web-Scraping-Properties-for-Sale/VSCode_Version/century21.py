2f129935e3823898ce43a316e9645fb6ff2df053
xroadtraveler <xroadtraveler@gmail.com>
2023-02-10 18:00:29 -0800

    add functionality to extract house price, address, beds, baths, and sqft

--- _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
+++ _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
@@ -8,4 +8,30 @@ soup=BeautifulSoup(c,"html.parser")
 
 all=soup.find_all("div",{"class":"propertyRow"})
 
-all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
+# all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
+
+for item in all:
+    print(item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ",""))
+    print(item.find_all("span",{"class":"propAddressCollapse"})[0].text)
+    print(item.find_all("span",{"class":"propAddressCollapse"})[1].text)
+    try:
+        print(item.find("span",{"class":"infoBed"}).find("b").text)
+    except:
+        print(None)
+    
+    try:
+        print(item.find("span",{"class":"infoSqFt"}).find("b").text)
+    except:
+        print(None)
+    
+    try:
+        print(item.find("span",{"class":"infoValueFullBath"}).find("b").text)
+    except:
+        print(None)
+    
+    try:
+        print(item.find("span",{"class":"infoValueHalfBath"}).find("b").text)
+    except:
+        print(None)
+    
+    print(" ")
