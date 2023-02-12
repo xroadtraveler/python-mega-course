c12b11cf6de55792a9862ef88817e4a5516bd3aa
xroadtraveler <xroadtraveler@gmail.com>
2023-02-11 17:01:47 -0800

    add code to search for and extract property lot size

--- _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
+++ _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
@@ -34,4 +34,11 @@ for item in all:
     except:
         print(None)
     
+    for column_group in item.find_all("div", {"class":"columnGroup"}):
+        # print(column_group)
+        for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}),column_group.find_all("span",{"class":"featureName"})):
+            # print(feature_group.text, feature_name.text)
+            if "Lot Size" in feature_group.text:
+                print(feature_name.text)
+    
     print(" ")
