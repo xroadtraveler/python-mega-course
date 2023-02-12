1840a4c899cd463137dbf19df4f58c5d55a9b0e5
xroadtraveler <xroadtraveler@gmail.com>
2023-02-11 17:43:20 -0800

    save data to pandas dataframe, remove test print statements for data

--- _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
+++ _Python-Apps/App-7_Web-Scraping-Properties-for-Sale/VSCode_Version/century21.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas
 
 r=requests.get("https://pythonizing.github.io/data/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
 c=r.content
@@ -10,35 +11,45 @@ all=soup.find_all("div",{"class":"propertyRow"})
 
 # all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
 
+l=[]
 for item in all:
-    print(item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ",""))
-    print(item.find_all("span",{"class":"propAddressCollapse"})[0].text)
-    print(item.find_all("span",{"class":"propAddressCollapse"})[1].text)
+    d={}
+    d["Address"]=item.find_all("span",{"class":"propAddressCollapse"})[0].text
+    d["Locality"]=item.find_all("span",{"class":"propAddressCollapse"})[1].text
+    d["Price"]=item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
     try:
-        print(item.find("span",{"class":"infoBed"}).find("b").text)
+        d["Beds"]=item.find("span",{"class":"infoBed"}).find("b").text
     except:
-        print(None)
+        d["Beds"]=None
     
     try:
-        print(item.find("span",{"class":"infoSqFt"}).find("b").text)
+        d["Area"]=item.find("span",{"class":"infoSqFt"}).find("b").text
     except:
-        print(None)
+        d["Area"]=None
     
     try:
-        print(item.find("span",{"class":"infoValueFullBath"}).find("b").text)
+        d["Full Baths"]=item.find("span",{"class":"infoValueFullBath"}).find("b").text
     except:
-        print(None)
+        d["Full Baths"]=None
     
     try:
-        print(item.find("span",{"class":"infoValueHalfBath"}).find("b").text)
+        d["Half Baths"]=item.find("span",{"class":"infoValueHalfBath"}).find("b").text
     except:
-        print(None)
+        d["Half Baths"]=None
     
     for column_group in item.find_all("div", {"class":"columnGroup"}):
         # print(column_group)
         for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}),column_group.find_all("span",{"class":"featureName"})):
             # print(feature_group.text, feature_name.text)
             if "Lot Size" in feature_group.text:
-                print(feature_name.text)
-    
-    print(" ")
+                d["Lot Size"]=feature_name.text
+    l.append(d)
+
+# print(l)
+# print(len(l))
+
+
+df=pandas.DataFrame(l)
+#print(df)
+
+df.to_csv("Output.csv")
