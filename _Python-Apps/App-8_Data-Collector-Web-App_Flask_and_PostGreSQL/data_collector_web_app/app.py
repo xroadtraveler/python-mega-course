9280d4854cfb363f59b650e822e9c755b79ba520
xroadtraveler <xroadtraveler@gmail.com>
2023-02-17 15:33:57 -0800

    add email-sending functionality on successful registration

--- _Python-Apps/App-8_Data-Collector-Web-App_Flask_and_PostGreSQL/data_collector_web_app/app.py
+++ _Python-Apps/App-8_Data-Collector-Web-App_Flask_and_PostGreSQL/data_collector_web_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/height_collector'
@@ -24,8 +25,8 @@ def success():
     if request.method=='POST':
         email=request.form["email_name"]
         height=request.form["height_name"]
-        print(email)
-        print(height)
+#        print(email, height)
+        send_email(email, height)
         if db.session.query(Data).filter(Data.email_ == email).count() == 0:
             data=Data(email, height)
             db.session.add(data)
