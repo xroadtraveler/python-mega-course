8e7346af6832b060bc8eceebe0df267327ceace3
xroadtraveler <xroadtraveler@gmail.com>
2023-03-25 16:41:21 -0700

    add conditional to filter out non-existing words

--- _Python-Apps/Bonus-App-1_English-Thesaurus/English_Thesaurus/app.py
+++ _Python-Apps/Bonus-App-1_English-Thesaurus/English_Thesaurus/app.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def define(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn’t exist. Please double check it."
 
 word = input("Enter word: ")
 
