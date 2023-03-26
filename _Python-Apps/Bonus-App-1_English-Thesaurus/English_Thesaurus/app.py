49289747a33cf2d2ea62fec8ae9a3376554c2722
xroadtraveler <xroadtraveler@gmail.com>
2023-03-26 15:06:24 -0700

    improve documentation for if-else statements

--- _Python-Apps/Bonus-App-1_English-Thesaurus/English_Thesaurus/app.py
+++ _Python-Apps/Bonus-App-1_English-Thesaurus/English_Thesaurus/app.py
@@ -7,9 +7,15 @@ data = json.load(open("data.json"))
 # Searches 'data' for compatible matches, returns definitions of input word
 def define(word):
     word = word.lower()
+
+    # Do first-level check for exact word matches
     if word in data:
         return data[word]
+    
+    # Search similar words in the case of a typo
+    # Check that similar words exist by checking for empty returned lists
     elif len(get_close_matches(word, data.keys())) > 0:
+        # Set user input (Y/N) to a variable, prompt user to confirm whether suggested word was their word
         yn = input("Did you mean %s instead? Enter 'Y' if yes, or 'N' if no: " % get_close_matches(word, data.keys(), cutoff=0.8)[0])
         if yn.upper() == "Y":
             return data[get_close_matches(word, data.keys(), cutoff=0.8)[0]]
