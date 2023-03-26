bd0d639b3e33fbecf94f505e47149cb58649298d
xroadtraveler <xroadtraveler@gmail.com>
2023-03-25 18:30:09 -0700

    add functionality to find most-similar word (such as in a typo situation) and ask user if they meant that

--- _Python-Apps/Bonus-App-1_English-Thesaurus/English_Thesaurus/app.py
+++ _Python-Apps/Bonus-App-1_English-Thesaurus/English_Thesaurus/app.py
@@ -8,6 +8,8 @@ def define(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys(), cutoff=0.8)[0]
     else:
         return "The word doesn’t exist. Please double check it."
 
