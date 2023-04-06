6826d254c62bcde1467112855c8c6f59fdb639eb
xroadtraveler <xroadtraveler@gmail.com>
2023-04-06 13:09:35 -0700

    set up basic script for time-based website blocker app

---
+++ _Python-Apps/Bonus-App-2_Website-Blocker/Website_Blocker/website_blocker.py
@@ -0,0 +1,14 @@
+import time
+
+# Windows filepath:
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+# Mac/Linux filepath:
+# hosts_path = "/etc/hosts"
+
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com",
+                "youtube.com", "www.youtube.com"]
+
+while True:
+    print(1)
+    time.sleep(5)
