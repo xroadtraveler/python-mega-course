d5ecd28f2d322d7fa218ca81c25d780177910d58
xroadtraveler <xroadtraveler@gmail.com>
2023-04-06 15:23:49 -0700

    add functionality to remove blocked websites from 'hosts' file outside of work hours, reorganize/add pointer variables for test/prod

--- _Python-Apps/Bonus-App-2_Website-Blocker/Website_Blocker/website_blocker.py
+++ _Python-Apps/Bonus-App-2_Website-Blocker/Website_Blocker/website_blocker.py
@@ -1,11 +1,17 @@
 import time
 from datetime import datetime as dt
 
+# Filepath of test version of 'hosts' file
 hosts_temp = "hosts.txt"
+
 # Windows filepath:
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts.txt"
+
 # Mac/Linux filepath:
 # hosts_path = "/etc/hosts"
+
+# Toggle/point towards test or real 'hosts' file
+hosts_pointer = hosts_path
 
 # Variables for 'hosts' file modification
 redirect = "127.0.0.1"
@@ -17,16 +23,23 @@ while True:
     year = dt.now().year
     month = dt.now().month
     today = dt.now().day
-    if dt(year, month, today, 8) < dt.now() < dt(year, month, today, 16):
+    if dt(year, month, today, 16) < dt.now() < dt(year, month, today, 17):
         print("Working hours...")
-        with open(hosts_temp, 'r+') as file:
+        with open(hosts_pointer, 'r+') as file:
             content=file.read()
-            print(content)
+            #print(content)
             for website in website_list:
                 if website in content:
                     pass
                 else:
                     file.write("\n" + redirect + " " + website)
     else:
+        with open(hosts_pointer, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("Fun hours...")
     time.sleep(5)
