b0ed2dc682d04c014c4205e5c0133cc8919b5497
xroadtraveler <xroadtraveler@gmail.com>
2023-04-06 13:41:42 -0700

    add time-checking functionality to 'website_blocker' app

--- _Python-Apps/Bonus-App-2_Website-Blocker/Website_Blocker/website_blocker.py
+++ _Python-Apps/Bonus-App-2_Website-Blocker/Website_Blocker/website_blocker.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime as dt
 
 # Windows filepath:
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
@@ -10,5 +11,11 @@ website_list = ["www.facebook.com", "facebook.com",
                 "youtube.com", "www.youtube.com"]
 
 while True:
-    print(1)
+    year = dt.now().year
+    month = dt.now().month
+    today = dt.now().day
+    if dt(year, month, today, 8) < dt.now() < dt(year, month, today, 16):
+        print("Working hours...")
+    else:
+        print("Fun hours...")
     time.sleep(5)
